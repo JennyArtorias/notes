c77269bcc0dd619a077c8c80c9857066a8e015bf
Artorias <jennyArtorias@gmail.com>
2016-04-16 17:48:36 +1200

    re:form validation notes added

---
+++ fCC.js
@@ -0,0 +1,36 @@
+Free code camp notes 
+
+Assignment always goes from right to left. Everything to the right of the = operator is resolved before the value is assigned to the variable to the left of the operator.
+
+i++;
+
+is the equivalent of
+
+i = i + 1; // i++
+
+
+Usage
+In mathematics, a number can be checked even or odd by checking the remainder of the division of the number by 2.
+
+17 % 2 = 1 (17 is Odd)
+48 % 2 = 0 (48 is Even
+            
+            
+            
+            myVar = myVar + 5;
+
+to add 5 to myVar. Since this is such a common pattern, there are operators which do both a mathematical operation and assignment in one step.
+
+One such operator is the += operator.
+
+myVar += 5; will add 5 to myVar.
+            
+            
+            "your name" is called a string literal. It is a string because it is a series of zero or more characters enclosed in single or double quotes.
+            
+            
+            
+            
+" or ' inside of your string?
+
+In JavaScript, you can escape a quote from considering it as an end of string quote by placing a backslash (\) in front of the quote.
