3b69b84df70f57960c38a43e71be22b173d8faeb
Artorias <jennyArtorias@gmail.com>
2016-04-16 19:56:28 +1200

    add:JQuery basics notes

---
+++ jQuery01.js
@@ -0,0 +1,29 @@
+<script src = "jquery-1.11.3.js"></script>
+<script src = "Javascript.js"></script>
+
+Dom tree represent document
+First jQuery code 
+-----------------------------------------
+    
+$(document).ready(function(){
+    
+    $("#right, #center").hide();
+    
+    
+    
+    
+});
+
+//READY waits to jquery code dled b4 Jquery executed
+//% -> jQuery -> Creates object for us to use
+// CSS selectors . -> access properties and methods (jQUerymethod)
+// $("") > basic format -> selector selector (elements)
+// JQUERY METHOD AND PROPERTIES
+// jquery vs javascript ( full version) vs shortcut
+
+// , -> css selector multiple
+
+Learning selectors and methods exclusive shortcut jQuery.
+
+1. SELECTORS $("").xxx();
+2. METHODS, PROPERTIES 
