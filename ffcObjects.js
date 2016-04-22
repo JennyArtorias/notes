9350e4f747d492ffc11e763f3178642c8043dc65
Artorias <jennyArtorias@gmail.com>
2016-04-22 19:39:16 +1200

    clear

--- ffcObjects.js
+++ ffcObjects.js
@@ -1,3 +1,13 @@
 reduce // previousVal[] currentVal = last element in teh array 
 
 sort // array sorting a - b b - a  0 return  asceneding descending
+
+string(thig) -> thing Anything to be converted to a string.
+
+\\ 
+
+\ escape 
+
+.replace([], "") 
+
+indexOf("xxx", "")
