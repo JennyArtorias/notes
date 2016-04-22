145c9fc9c12cd8b253ff01d9ecc7924270974391
Artorias <jennyArtorias@gmail.com>
2016-04-22 19:53:25 +1200

    re:selectors

--- jQuerynotes.js
+++ jQuerynotes.js
@@ -7,3 +7,8 @@ $(document).ready(function(){
 
 document ready function, your code may run before your HTML is rendered, which would cause bugs.
 make sure HTML rendered > javascript run
+
+jQuery selectrs an HTML ELEMENT -> selector then does something to that element 
+
+$ < -- selector
+
